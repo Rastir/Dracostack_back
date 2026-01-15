dbe7c844eb364b1e842e5046b0da066a8221bdb0
Rastir <aldojero@hotmail.com>
2026-01-14 20:59:23 -0500

    test 7

--- src/main/java/com/churninsight/api/dto/PredictionResponseDTO.java
+++ src/main/java/com/churninsight/api/dto/PredictionResponseDTO.java
@@ -2,13 +2,10 @@ package com.churninsight.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class PredictionResponseDTO {
 
     private int prediction;
